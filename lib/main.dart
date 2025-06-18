3f935c744ecd2a273a2644046127f0f34a248b95
ayan-riaz <ayanriazf@gmail.com>
2025-06-18 22:33:41 +0500

    fix: removed Stripe secret key from code and cleaned up

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
-import 'package:food_app/const.dart';
 import 'package:food_app/services/notification_services.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:google_fonts/google_fonts.dart';
